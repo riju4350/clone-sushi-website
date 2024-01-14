5ab42fba93cc1fa8dba7f0771f80cd48cc754523
riju <riju4350@gmail.com>
2024-01-14 23:16:48 +0600

    navbar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,31 @@
 /*=============== SHOW MENU ===============*/
+const navToggle = document.getElementById("toggle-button");
+const navMenu = document.getElementById("nav-menu");
+const navClose = document.getElementById("close-button");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 
-
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+}
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLinks = document.querySelectorAll(".nav-link");
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+navLinks.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
